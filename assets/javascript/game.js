68b0d3bf85d6d19bf77cf58c24942ce2c90d25bc
jeremyhe <jeremyhe1@gmail.com>
2017-06-09 17:48:55 -0400

    new stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,20 @@ var backgrounds = ['cafe', 'bar', 'table', 'patio'];
 generateOptions(backgrounds, $('#backgrounds'), 'backgroundOptions');
 
 //Character list
-var characters = ['polarBear', 'panda', 'penguin'];
-generateOptions(characters, $('#characters'), 'characterOptions');
+var characters = {
+    cafe: ['polarBear', 'panda', 'penguin'],
+    bar: ['polarBear', 'panda', 'penguin'],
+    table: ['panda'],
+    patio: ['penguin'],
+};
 
 //Tracks your current location
 var currentLocation = 'cafe';
 setBackground(currentLocation);
+generateOptions(characters[currentLocation], $('#characters'), 'characterOptions');
+
+//Call this to reattach onclick to the button classes
+resetCharacterButtons();
 
 //Tracks if you're in a conversation
 var talking = false;
@@ -18,84 +26,94 @@ var talking = false;
 var polarBear = {
     name: 'Polar Bear',
     species: 'polar bear',
-    relation: 0,
+    affection_lvl: 0,
     startConversation: function() {
-        if (this.relation === 0) {
+        if (this.affection_lvl === 0) {
             return ("Hi there! My name is Polar Bear, welcome to my cafe!");
         }
-        else if (this.relation > 0 && this.relation < 3) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
             return ("Welcome!");
-        } else if (this.relation >= 3 && this.relation < 10) {
+        } else if (this.affection_lvl >= 3 && this.affection_lvl < 10) {
             return ("It's good to see you again!");
         } else {
             return ("Hello friend! Would you like the usual?");
         }
     }
-}
+};
 
 var panda = {
     name: 'Panda',
     species: 'giant panda',
-    relation: 0,
+    affection_lvl: 0,
     startConversation: function() {
-        if (this.relation === 0) {
-            return ("Hey hey! I'm Panda! I'm a regular here. Aren't I cute?");
+        if (this.affection_lvl === 0) {
+            return ("Hey! I'm Panda! I'm a regular here. Aren't I cute?");
         }
-        else if (this.relation > 0 && this.relation < 3) {
-            return ("Whos are you again?");
-        } else if (this.relation >= 3 && this.relation < 10) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
+            return ("I wonder when my food will arrive?");
+        } else if (this.affection_lvl >= 3 && this.affection_lvl < 10) {
             return ("Oh hey, didn't see you there.");
         } else {
             return ("Hey hey! Listen to this!");
         }
     }
-}
+};
 
 var penguin = {
     name: 'Penguin',
     species: 'penguin',
-    relation: 0,
+    affection_lvl: 0,
     startConversation: function() {
-        if (this.relation === 0) {
+        if (this.affection_lvl === 0) {
             return ("Hello. I'm penguin. Nice to meet you!");
         }
-        else if (this.relation > 0 && this.relation < 3) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
             return ("Hey again.");
-        } else if (this.relation >= 3 && this.relation < 10) {
+        } else if (this.affection_lvl >= 3 && this.affection_lvl < 10) {
             return ("Will I ever pass the driving exam?");
         } else {
             return ("I got my license!");
         }
     }
-}
+};
 //Sets background scene
 function setBackground(inputValue) {
-    console.log(inputValue);
     var imageName = inputValue + '.jpg';
-    var imageLocation = 'url(assets/images/' + imageName + ')';
-            $('body').css('background-image', imageLocation);
-            $('body').css('background-size', '100%, auto');
-            $('body').css('background-repeat', 'no-repeat');
-            }
+    var imagePath = "url(assets/images/" + imageName + ")";
 
-            //Generates button options from chosen array, then appends them to a selected element
-            function generateOptions(inputArray, outputPlace, classOption) {
-                outputPlace.empty();
-                inputArray.forEach(function(item) {
-                    var newOption = $('<button>');
-                    newOption.addClass(classOption);
-                    newOption.data('value', item);
-                    newOption.text(item.toUpperCase());
-                    //console.log(newOption.data('value'));
-                    newOption.appendTo(outputPlace);
-                });
-            }
+    var body = $('body');
+    body.css('background-image', imagePath);
+    //$('body').css('background-size', '100%, auto');
+    body.css('background-repeat', 'no-repeat')
 
-            //Conversation event handler
-            function enterConversation(inputValue) {
-                console.log(inputValue);
-                generateConversation(inputValue);
-            }
+}
+
+//Generates button options from chosen array, then appends them to a selected element
+function generateOptions(inputArray, outputPlace, classOption) {
+    outputPlace.empty();
+    inputArray.forEach(arrayLoop);
+    function arrayLoop(item) {
+        var newOption = $('<button>');
+        newOption.addClass(classOption);
+        if (classOption === 'characterOptions') {
+            newOption.addClass('btn btn-success');
+        } else if (classOption === 'backgroundOptions') {
+            newOption.addClass('btn btn-primary');
+        } else {
+            newOption.addClass('btn btn-default');
+        }
+        newOption.data('value', item);
+        newOption.text(item.toUpperCase());
+        //console.log(newOption.data('value'));
+        newOption.appendTo(outputPlace);
+    }
+}
+
+//Conversation event handler
+function enterConversation(inputValue) {
+    //console.log(inputValue);
+    generateConversation(inputValue);
+}
 
 //Selects character to generate conversation with
 function generateConversation(inputValue) {
@@ -114,57 +132,72 @@ function generateConversation(inputValue) {
     }
 }
 
+//Opens stats
+generateAllStats();
 
-//WARNING INCOMPLETE CODE START <---------
 //Generates character stats
 function generateCharacterStats(inputObj) {
     var setCharacter = $('<div>');
-    setCharacter.addClass('col-sm-4');
+    setCharacter.addClass('col-sm-4 panel panel-info');
+    var setCharacterHeader = $('<div>');
+    setCharacterHeader.addClass('panel-heading');
+    var panelTitle = $('<h3>');
+    panelTitle.addClass('panel-title');
+    panelTitle.text(inputObj['name']);
+    setCharacterHeader.append(panelTitle);
+    var setCharacterBody = $('<div>');
+    setCharacterBody.addClass('panel-body');
     for (x in inputObj) {
         var value = inputObj[x];
         //console.log(typeof value);
         if (typeof value != 'function') {
             var characterInfo = $('<p>');
-            characterInfo.text = x.toUpperCase() + ': ' + inputObj[x];
-            console.log(characterInfo.text);
-            setCharacter.append(characterInfo);
+            characterInfo.text(x.toUpperCase() + ': ' + inputObj[x]);
+            //console.log(characterInfo.text);
+            setCharacterBody.append(characterInfo);
         }
     }
+    setCharacter.append(setCharacterHeader);
+    setCharacter.append(setCharacterBody);
     setCharacter.appendTo($('#characterStats'));
 }
 
+//Clears all stuff from $('#characterStats');
 function clearStats() {
-    $('characterStats').empty();
+    $('#characterStats').empty();
 }
 
-generateCharacterStats(polarBear);
-generateCharacterStats(panda);
-generateCharacterStats(penguin);
+function generateAllStats() {
+    generateCharacterStats(polarBear);
+    generateCharacterStats(panda);
+    generateCharacterStats(penguin);
+}
 //------------> INCOMPLETE SEGMENT END
 
 function callPolarBear() {
     alert(polarBear.startConversation());
 
-    polarBear.relation++;
+    polarBear.affection_lvl++;
     talking = false;
 }
 
 function callPanda() {
     alert(panda.startConversation());
 
-    panda.relation++;
+    panda.affection_lvl++;
     talking = false;
 }
 
 function callPenguin() {
     alert(penguin.startConversation());
 
-    penguin.relation++;
+    penguin.affection_lvl++;
     talking = false;
 }
 
 //Choses background from assets/images
-$('.backgroundOptions').on('click', function() {
+var backgroundOptions = $('.backgroundOptions');
+backgroundOptions.on('click', function() {
     //console.log($(this).data('value'));
     if(currentLocation === $(this).data('value')) {
         return;
@@ -172,14 +205,27 @@ $('.backgroundOptions').on('click', function() {
     currentLocation = $(this).data('value');
     //console.log(currentLocation);
     setBackground(currentLocation);
+    generateOptions(characters[currentLocation], $('#characters'), 'characterOptions');
+    resetCharacterButtons();
 });
 
-$('.characterOptions').on('click', function() {
-    if (talking) {
-        return;
-    } else {
-        talking = true;
-    }
-    var currentCharacter = $(this).data('value');
-    enterConversation(currentCharacter);
-});
+//This needs to be called each time options are generated because .empty removes it
+function resetCharacterButtons() {
+    //console.log(backgroundOptions);
+
+    var characterOptions = $('.characterOptions');
+    characterOptions.on('click', function() {
+        //console.log('Before talk check');
+        if (talking) {
+            return;
+        } else {
+            talking = true;
+        }
+        //console.log('After talk check');
+        var currentCharacter = $(this).data('value');
+        enterConversation(currentCharacter);
+        talking = false;
+        clearStats();
+        generateAllStats();
+    });
+}
