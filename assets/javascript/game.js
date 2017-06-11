d2bdbd1150f87ad01cab202f0b98ec54bbd93e23
jeremyhe <jeremyhe1@gmail.com>
2017-06-11 13:12:50 -0400

    ready for beta testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,8 @@ generateOptions(backgrounds, $('#backgrounds'), 'backgroundOptions');
 var characters = {
     cafe: ['polarBear', 'panda', 'penguin'],
     bar: ['polarBear', 'panda', 'penguin'],
-    table: ['panda'],
-    patio: ['penguin'],
+    table: ['penguin'],
+    patio: ['panda'],
 };
 
 //Clears portrait
@@ -36,26 +36,40 @@ var polarBear = {
         if (this.affection_lvl === 0) {
             return ["Hi there! My name is Polar Bear, welcome to my cafe!"];
         }
-        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 5) {
             return ["Welcome!"];
-        } else if (this.affection_lvl >= 3 && this.affection_lvl < 6) {
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
             return ["It's good to see you again!"];
         } else {
             return ["Hello friend!",
-                "Would you like the usual?",
-                "I got some fresh ground coffee.",
-                "It just came in today!"];
+                "Would you like the usual?"];
+        }
+    },
+    cafe: function() {
+        if (this.affection_lvl > 9) {
+            return ["It's always good to see a familiar face.",
+                "Sometimes this place doesn't have many patrons,",
+                "and it feels rather lonely.",
+                "Just so you know,",
+                "you will always be welcome here."];
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
+            return ["You've been coming more often.",
+                "I wonder if you've taken a liking to this place.",
+                "Please enjoy your stay."];
+        } else {
+            return ["Surprised to see a polar bear running this cafe?"];
         }
     },
     initialConversation1: function() {
         var speech = ['Would you like to order something?.'];
-        initiateConversation(speech);
+        //initiateConversation(speech);
         return 'pb1';
     },
     responseConversation1: function(input) {
         switch (input) {
             case '0':
-                return ["I see.", "I'll leave you to it then."];
+                return ["I see.",
+                "I'll leave you to it then."];
                 break;
             case '1':
                 return ["Coming right up!"];
@@ -67,7 +81,7 @@ var polarBear = {
     },
     initialConversation2: function() {
         var speech = ['The weather today sure is nice.', 'I hope you are well.'];
-        initiateConversation(speech);
+        //initiateConversation(speech);
         return 'pb2';
     },
     responseConversation2: function(input) {
@@ -93,14 +107,38 @@ var panda = {
         if (this.affection_lvl === 0) {
             return (["Hey! I'm Panda! I'm a regular here. Aren't I cute?"]);
         }
-        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 5) {
             return ["I wonder when my food will arrive?"];
-        } else if (this.affection_lvl >= 3 && this.affection_lvl < 6) {
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
             return ["Oh hey, didn't see you there."];
         } else {
+            return ["I hope you are having a great day!"];
+        }
+    },
+    cafe: function() {
+        if (this.affection_lvl > 9) {
             return ["Hey hey! Listen to this!",
-                    "Today at the zoo, some elementary school students came on a field trip.",
-                    "I gave them lots of service!"];
+                    "Today at the zoo,",
+                    "some elementary school students came on a field trip.",
+                    "I put on a great show service for them!"];
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
+            return ["You like talking to me don't you?", "I wonder if this means we're friends."];
+        } else {
+            return ["So today at the zoo..."];
+        }
+    },
+    patio: function() {
+        if (this.affection_lvl > 9) {
+            return ["I appreciate you talking with me.",
+                "It makes me feel all warm and fuzzy.",
+                "Like basking in the sun!"];
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
+            return ["When I first came here...",
+                "This cafe didn't serve bamboo.",
+                "Can you believe that?"];
+        } else {
+            return ["Normally I would be sitting inside,",
+                "but being outside is nice too sometimes!"];
         }
     }
 };
@@ -113,13 +151,43 @@ var penguin = {
         if (this.affection_lvl === 0) {
             return (["Hello. I'm penguin. Nice to meet you!"]);
         }
-        else if (this.affection_lvl > 0 && this.affection_lvl < 3) {
+        else if (this.affection_lvl > 0 && this.affection_lvl < 5) {
             return (["Hey again."]);
-        } else if (this.affection_lvl >= 3 && this.affection_lvl < 6) {
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
             return (["Will I ever pass the driving exam?"]);
         } else {
             return (["I got my license!",
                     "I'm going to ask Ms. Penko out for a drive!"]);
+        }
+    },
+    cafe: function() {
+        if (this.affection_lvl > 9) {
+            return ["I appreciate that you always listen to me.",
+                    "Because of your support,",
+                    "I passed the driving exam!",
+                    "I finally have a driver's liscense!"];
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
+            return ["I wonder if I'll do better this time?",
+                "Oh well it's like they say,",
+                "you can't succeed if you don't try."];
+        } else {
+            return ["Is there something you need?",
+                "I can't really help you right now."];
+        }
+    },
+    table: function() {
+        if (this.affection_lvl > 9) {
+            return ["I appreciate that you always listen to me.",
+                    "Because of your support,",
+                    "I passed the driving exam!",
+                    "I finally have a driver's liscense!"];
+        } else if (this.affection_lvl >= 5 && this.affection_lvl <= 9) {
+            return ["I won't give up on the driving exam!",
+                "It's like they say,",
+                "you can't succeed if you don't try."];
+        } else {
+            return ["Sometimes I like this quiet corner",
+                "It helps me think."];
         }
     }
 };
@@ -214,6 +282,11 @@ function clearStats() {
     $('#characterStats').empty();
 }
 
+function refreshStats () {
+    clearStats();
+    generateAllStats();
+}
+
 function generateAllStats() {
     generateCharacterStats(polarBear);
     generateCharacterStats(panda);
@@ -234,7 +307,7 @@ function sentencePackage(input) {
 
 function panelPackage(input) {
     var newPanel = $('<div>');
-    newPanel.addClass('panel-body');
+    newPanel.addClass('panel-body w3-container w3-animate-opacity');
     newPanel.append(input);
     return newPanel;
 }
@@ -247,10 +320,9 @@ function callPolarBear() {
         talking = true;
     }
     talkingTo = 'polarBear';
-    characterImage.css('background-image', 'url(./assets/images/polarBear1.jpg)');
-    $('#conversationWords').css('visibility', 'visible');
-    initiateConversation(polarBear.startConversation());
+    initiateConversation(polarBear.startConversation(), talkingTo);
 
+    checkLocationConversation();
     polarBear.affection_lvl++;
     //talking = false;
 }
@@ -261,10 +333,10 @@ function callPanda() {
     } else {
         talking = true;
     }
-    characterImage.css('background-image', 'url(./assets/images/panda1.jpg)');
-    $('#conversationWords').css('visibility', 'visible');
-    initiateConversation(panda.startConversation());
+    talkingTo = 'panda';
+    initiateConversation(panda.startConversation(), talkingTo);
 
+    checkLocationConversation();
     panda.affection_lvl++;
     //talking = false;
 }
@@ -276,10 +348,9 @@ function callPenguin() {
         talking = true;
     }
     talkingTo = 'penguin';
-    characterImage.css('background-image', 'url(./assets/images/penguin1.jpg)');
-    $('#conversationWords').css('visibility', 'visible');
-    initiateConversation(penguin.startConversation());
+    initiateConversation(penguin.startConversation(), talkingTo);
 
+    checkLocationConversation();
     penguin.affection_lvl++;
     //talking = false;
 }
@@ -348,12 +419,13 @@ function createNextButton(conversationHolder) {
 function setNextButton() {
     $('#buttonNext').on('click', function() {
         if (conversationHolder.dialoguePlace >= conversationHolder.dialogueLength) {
-            $('#nextButton').empty();
+            //$('#nextButton').empty();
             $('#conversationWords').empty();
             characterImage.css('background-image', 'none');
             $('#conversationWords').css('visibility', 'hidden');
             talking = false;
-            checkTalkingTo();
+            refreshStats();
+            $(this).remove();
         } else {
             handleConversation();
         }
@@ -389,6 +461,7 @@ backgroundOptions.on('click', function() {
     playAudio(currentLocation);
     setBackground(currentLocation);
     generateOptions(characters[currentLocation], $('#characters'), 'characterOptions');
+    checkLocationConversation();
     resetCharacterButtons();
 });
 
@@ -408,13 +481,171 @@ function resetCharacterButtons() {
         var currentCharacter = $(this).data('value');
         enterConversation(currentCharacter);
         //talking = false;
-        clearStats();
-        generateAllStats();
+        //refreshStats();
     });
 }
 
-function initiateConversation(input) {
-    loadConversation(input);
+function initiateConversation(inputConversation, inputCharacter) {
+    setCharacterBackground(inputCharacter);
+    loadConversation(inputConversation);
     createConversation(conversationHolder);
 }
 
+function setCharacterBackground(inputCharacter) {
+    switch (inputCharacter) {
+        case 'polarBear':
+            characterImage.css('background-image', 'url(./assets/images/polarBear1.jpg)');
+            $('#conversationWords').css('visibility', 'visible');
+            break;
+        case 'panda':
+            characterImage.css('background-image', 'url(./assets/images/panda1.jpg)');
+            $('#conversationWords').css('visibility', 'visible');
+            break;
+        case 'penguin':
+            characterImage.css('background-image', 'url(./assets/images/penguin1.jpg)');
+            $('#conversationWords').css('visibility', 'visible');
+            break;
+        default:
+            break;
+    }
+}
+
+function checkLocationConversation() {
+    var additionalOptions = $('#conversationOptions');
+    additionalOptions.empty();
+    var type = 'locationConversation';
+    switch (currentLocation) {
+        case 'cafe':
+            characters.cafe.forEach(function(input) {
+                if (checkForKey(input, 'cafe') && generateRandomKey(.3)) {
+                    additionalOptions.append(packageButton(type, input, input));
+                } else {
+                    //console.log('Character ' + input + ' does not have this key.');
+                }
+            });
+            break;
+        case 'bar':
+            characters.bar.forEach(function(input) {
+                if (checkForKey(input, 'bar') && generateRandomKey(.3)) {
+                    additionalOptions.append(packageButton(type, input, input));
+                } else {
+                    //console.log('Character ' + input + ' does not have this key.');
+                }
+            });
+            break;
+        case 'table':
+            characters.table.forEach(function(input) {
+                if (checkForKey(input, 'table') && generateRandomKey(.3)) {
+                    additionalOptions.append(packageButton(type, input, input));
+                } else {
+                    //console.log('Character ' + input + ' does not have this key.');
+                }
+            });
+            break;
+        case 'patio':
+            characters.patio.forEach(function(input) {
+                if (checkForKey(input, 'patio') && generateRandomKey(.5)) {
+                    additionalOptions.append(packageButton(type, input, input));
+                } else {
+                    //console.log('Character ' + input + ' does not have this key.');
+                }
+            });
+            break;
+        default:
+            break;
+    }
+    setButtonByClass(type);
+}
+
+function generateRandomKey(inputChance) {
+    return (Math.random() < inputChance);
+}
+
+//Checks if key is in object
+function checkForKey(inputName, inputKey) {
+    var loadObject = selectCharacter(inputName);
+    return loadObject.hasOwnProperty(inputKey);
+}
+
+//Selects character object
+function selectCharacter(input) {
+    switch (input) {
+        case 'polarBear':
+            return polarBear;
+            break;
+        case 'panda':
+            return panda;
+            break;
+        case 'penguin':
+            return penguin;
+            break;
+        default:
+            break;
+    }
+}
+
+//Initiates a special conversation between you and a character
+function startLocationConversation(characterInput, locationInput) {
+    var character = selectCharacter(characterInput);
+    switch(locationInput) {
+        case 'cafe':
+            initiateConversation(character.cafe(), characterInput);
+            break;
+        case 'bar':
+            initiateConversation(character.bar(), characterInput);
+            break;
+        case 'table':
+            initiateConversation(character.table(), characterInput);
+            break;
+        case 'patio':
+            initiateConversation(character.patio(), characterInput);
+            break;
+        default:
+            break;
+    }
+}
+
+function packageButton(inputClass, inputValue, inputCharacter) {
+    var newButton = $('<button>');
+    var classValue = 'btn btn-default ' + inputClass;
+    newButton.addClass(classValue);
+    newButton.attr('value', inputValue);
+    newButton.text('+' + inputCharacter.toUpperCase());
+    return newButton;
+}
+
+//Sets special conversation buttons
+function setButtonByClass(inputClass) {
+    switch (inputClass) {
+        case 'locationConversation':
+            setLocationConversationButton();
+            break;
+        case 'personalConversation':
+            setSpecialConversationButton();
+            break;
+        case 'responsiveConversation':
+            setSpecialConversationButton();
+            break;
+        default:
+            break;
+    }
+}
+
+//Special conversation: personalConversation
+function setLocationConversationButton() {
+    $('.locationConversation').on('click', function() {
+        var buttonValue = $(this).attr('value');
+        var updateCharacter = selectCharacter(buttonValue);
+        startLocationConversation(buttonValue, currentLocation);
+        updateCharacter['affection_lvl'] += 5;
+        //console.log('Runs and returns value: ' + $(this).attr('value'));
+        $(this).remove();
+    });
+}
+
+//Special conversation: responsiveConversation
+function setResponsiveConversationButton() {
+    $('.responsiveConverstion').on('click', function() {
+        $(this).remove();
+    });
+}
